24cf3bd62b94b0403f89d9312121d1a9e51167f8
lucasgreck <lucas.greck@gmail.com>
2020-09-12 16:15:02 +0200

    Ajout CrossOrigin

--- lending_management/src/main/java/com/lendingmanagement/controller/LendBookManagementController.java
+++ lending_management/src/main/java/com/lendingmanagement/controller/LendBookManagementController.java
@@ -7,15 +7,13 @@ import com.lendingmanagement.model.PostString;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 import java.text.SimpleDateFormat;
 
 @Controller
+@CrossOrigin
 public class LendBookManagementController {
 	
 	@Autowired
